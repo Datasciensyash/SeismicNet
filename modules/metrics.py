c08d9e1cd64749e9a81df8a98186166fb3ee26a3
Datasciensyash <60406311+Datasciensyash@users.noreply.github.com>
2020-02-06 15:41:41 +0300

    Update metrics.py

--- modules/metrics.py
+++ modules/metrics.py
@@ -1,4 +1,6 @@
 import torch
+import numpy as np
+
 def iou_pytorch(outputs: torch.Tensor, labels: torch.Tensor, SMOOTH=0.001, mean=True):
 
     intersection = (outputs & labels).float().sum((1, 2))  
@@ -10,3 +12,16 @@ def iou_pytorch(outputs: torch.Tensor, labels: torch.Tensor, SMOOTH=0.001, mean=
         return iou.mean()
     else:
         return iou
+
+def iou_numpy(outputs: np.array, labels: np.array, mean=True):
+    outputs = outputs.squeeze(1)
+    
+    intersection = (outputs & labels).sum((1, 2))
+    union = (outputs | labels).sum((1, 2))
+    
+    iou = (intersection + SMOOTH) / (union + SMOOTH)
+    
+    if mean:
+    	return iou.mean()  # Or thresholded.mean()
+    else:
+    	return iou
