5799ec5bc7bd1a8cd4158c0629cb6129e4f38a49
Datasciensyash <60406311+Datasciensyash@users.noreply.github.com>
2020-02-15 17:10:23 +0300

    New parameter for predictions

--- predict.py
+++ predict.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
 	parser.add_argument('--config')
 	parser.add_argument('--path')
-	parser.add_argument('-filename')		
+	parser.add_argument('--filename')		
 	args = parser.parse_args()
 
 	config = args.config
